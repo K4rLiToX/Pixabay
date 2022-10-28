80f2739f5886d088f374c58a41a381ac7ecc74ed
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2022-10-29 01:22:01 +0200

    fix: add imports

--- app/src/main/java/com/carlosdiestro/pixabay/ui/theme/Theme.kt
+++ app/src/main/java/com/carlosdiestro/pixabay/ui/theme/Theme.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import com.carlosdiestro.pixabay.core.ui.theme.*
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
