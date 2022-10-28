b2dc2ae14df417ab37fb400cdbdd9a30a49edb1f
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2022-10-29 01:17:36 +0200

    [Feature] Images (#1)

---
+++ app/src/main/java/com/carlosdiestro/pixabay/core/data/repositories/ImageRepositoryImpl.kt
@@ -0,0 +1,34 @@
+package com.carlosdiestro.pixabay.core.data.repositories
+
+import android.util.Log
+import com.carlosdiestro.pixabay.BuildConfig
+import com.carlosdiestro.pixabay.core.domain.Image
+import com.carlosdiestro.pixabay.core.framework.local.ImageDao
+import com.carlosdiestro.pixabay.core.framework.remote.PixabayApi
+import com.carlosdiestro.pixabay.images.domain.repositories.ImageRepository
+import com.carlosdiestro.pixabay.utils.toDomain
+import com.carlosdiestro.pixabay.utils.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class ImageRepositoryImpl @Inject constructor(
+    private val api: PixabayApi,
+    private val dao: ImageDao
+) : ImageRepository {
+
+    override fun getImagesByQuery(query: String): Flow<List<Image>> {
+        return dao.getAll().toDomain()
+            .map { images -> images.filter { img -> img.querySearch == query } }
+    }
+
+    override fun getImageById(id: Int): Flow<Image> {
+        return dao.getById(id).toDomain()
+    }
+
+    override suspend fun cacheImagesByQuery(query: String) {
+        Log.d("DEBUG", "cacheImagesByQuery:  $query")
+        val response = api.getImagesByQuery(BuildConfig.API_KEY, query)
+        dao.insert(response.images.toEntity(query))
+    }
+}
---
+++ app/src/main/java/com/carlosdiestro/pixabay/images/ui/models/SimpleImagePLO.kt
@@ -0,0 +1,9 @@
+package com.carlosdiestro.pixabay.images.ui.models
+
+class SimpleImagePLO(
+    val id: Int,
+    val thumbnail: String,
+    val userName: String,
+    val tags: String,
+    val querySearch: String
+)
